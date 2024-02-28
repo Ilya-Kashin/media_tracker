1843de7c8c8ba527fdef0cb438443548c542172d
Ilya Kashin <ilya.deferson@gmail.com>
2024-02-28 18:40:22 +0300

    edit script.sh

--- pythonProject/projectfolder/script.sh
+++ pythonProject/projectfolder/script.sh
@@ -4,4 +4,4 @@
 export PATH_MEDIA=/home/ilya/work_dir/media/
 export PATH_DUMP=/home/ilya/work_dir/dump/
 pip install -r requirements.txt
-python3 /home/ilya/PycharmProjects/media_tracker/pythonProject/projectfolder/src/tracker.py
+python3 /pythonProject/projectfolder/src/tracker.py
